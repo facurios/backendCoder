c6297b8e81b5138a7bf9c02573775f094f38933a
facurios <facuuni8@gmail.com>
2022-09-02 19:57:07 -0300

    Desafio 12- Websocket

---
+++ Clase12-WebSocket/class/productos.js
@@ -0,0 +1,73 @@
+const fs = require('fs/promises');
+
+class Productos {
+    constructor(name) {
+        this.fileName = name;
+        this.countID = 0;
+        this.content = [];
+        this.init();
+    }
+    //Método para inicializar el archivo
+    async init() {
+        try {
+			let data = await fs.readFile(this.fileName);
+			this.content = JSON.parse(data);
+			for (const element of this.content) {
+				if (element.id > this.countID) this.countID = element.id;
+			}
+		} catch (error) {
+			console.log('Aún no hay archivo');
+		}
+    }
+    //Método que escribe/sobreescribe
+    async write() {
+        await fs.writeFile(this.fileName, JSON.stringify(this.content));
+    }
+    //Método para guardar un producto
+    save(object) {
+        this.countID++; 
+        object["id"] = this.countID; 
+        this.content.push(object); 
+        this.write(); 
+        return `El id del objeto añadido es ${this.countID}`; 
+    }
+    //Método para obtener todos los productos
+    getAll() {
+        return this.content;
+    }
+    //Método para obtener un producto según ID
+    getById(id) {
+        let result;
+        if (this.content !== []) {
+            result = this.content.find(x => x.id === id);
+            if (result === undefined) {
+                result = null;
+            }
+        } else {
+            result = 'El archivo está vacío';
+        }
+        return result;
+    }
+    //Método para eliminar un producto según ID
+    deleteById(id) { 
+        let result;
+        if (this.content !== []) {
+            let newContent = this.content.filter(x => x.id !== id);
+            this.content = newContent;
+            this.write();
+            result = `El producto ${id} fue eliminado`;
+        } else {
+            result = `El archivo está vacío`;
+        }
+        return result;
+    }
+    //Método para actualizar productos
+    update(id, obj){
+        const index = this.content.findIndex(objT => objT.id == id);
+        obj.id = this.content[index].id;
+        this.content[index] = obj;
+        return obj;
+    }
+}
+
+module.exports = Productos;
---
+++ Clase12-WebSocket/server.js
@@ -0,0 +1,57 @@
+/* ---------------------- Modulos ----------------------*/
+const express = require('express');
+const morgan = require('morgan');
+const Productos = require('./class/productos.js');
+const Chats = require('./class/chats.js');
+
+/* ------------------- Instancia Server -------------------*/
+const app = express();
+const productos = new Productos(__dirname + '/DB/productos.json');
+const chats = new Chats(__dirname + '/DB/chats.json');
+/* ---------------------- Middlewares ----------------------*/
+app.use(express.static('./public')) 
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+
+/* ------------- Motor de Plantillas ---------------------- */
+app.set('views', './views/layouts');
+app.set('view engine', 'ejs');
+
+/* ---------------------- Rutas ----------------------*/
+app.get('/', (req, res) => {
+    let content = productos.content;
+    return res.render('index.ejs', {content});
+})
+
+app.post("/", (req, res) => {
+    productos.save(req.body);
+    let content = productos.content;
+    return res.render('index.ejs', {content});
+});
+
+/* ---------------------- Servidor ----------------------*/
+const PORT = 8888;
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}/`);
+});
+
+const io = require('socket.io')(server);
+
+server.on('error', error => console.log(`Error en servidor ${error}`));
+
+/* ---------------------- WebSocket ----------------------*/
+const messages = [
+    {author: 'example@gmail.com', text: 'Bienvenido al server', datetime: '31/08/2022, 20:30:45'}
+]
+
+io.on('connection', (socket) => {
+    console.log(`Nuevo cliente conectado! ${socket.id}`);
+    socket.emit('from-server-messages', chats.getAll());
+
+    socket.on('from-client-messages', (data) => {
+        data.time = new Date().toLocaleString();
+        chats.save(data);
+        io.sockets.emit('from-server-messages', [data]);
+    });
+})
