a06c669e4cdf4ee9e03280176d2234807aafa672
facurios <facuuni8@gmail.com>
2022-11-22 18:38:26 -0300

    clase-34-heroku

---
+++ clase-34-heroku/child_random.js
@@ -0,0 +1,14 @@
+process.on("message",data =>{
+    process.send(random(data))
+})
+
+function random(data){
+    console.log('me ejecuto')
+    let objeto = {}
+    for (let i = 0; i < parseInt(data); i++){
+        let clave = Math.floor(Math.random()*1000)+'' 
+        if (objeto[`${clave}`]) objeto[`${clave}`]++
+        else objeto[`${clave}`] = 1   
+    }
+    return objeto;
+}
